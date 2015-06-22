f5d76a7964db91a8342d86f42da92b6ef691fa25
Auktavian Garrett <augarret@cisco.com>
2015-06-22 00:02:35 +0000

    Added codecoverage support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,13 @@ require 'serverspec'
 
 set :backend, :exec
 
+require 'simplecov'
+require 'simplecov-json'
+require 'simplecov-rcov'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::JSONFormatter,
+  SimpleCov::Formatter::RcovFormatter,
+]
+SimpleCov.start
